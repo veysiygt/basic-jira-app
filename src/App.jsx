91985953f3d567528490ffb3cb6b3ccbb5e47e00
veysiygt <veysiygt21@gmail.com>
2023-06-19 13:48:11 +0300

    Json Server(Axios) was installed and useEffect was integrated into the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
+import axios from 'axios';
 
 function App() {
 
   const [tasks, setTasks] = useState([]);
   
-  const createTask = (title,desc) => {
-
-    const createdTasks = [
-      ...tasks,{
-        id: Math.round(Math.random()*999999),
-        title,
-        desc
-      }
-    ];
+  const createTask = async(title,desc) => {
+    const response = await axios.post('http://localhost:3001/tasks', {
+      title,
+      desc
+    });
+    const createdTasks = [...tasks,response.data];
 
     setTasks(createdTasks);
 
   }
 
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3001/tasks');
+    setTasks(response.data);
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   const deleteTaskById = (id) => {
+    axios.delete(`http://localhost:3001/tasks/${id}`);
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     });
@@ -29,6 +37,10 @@ function App() {
   }
 
   const editTaskById = (id,updatedTitle,updatedDesc) => {
+    axios.put(`http://localhost:3001/tasks/${id}`, {
+      title : updatedTitle,
+      desc : updatedDesc
+    });
     const updatedTasks = tasks.map((task) =>{
       if(task.id === id){
         return {id,title:updatedTitle,desc:updatedDesc}
